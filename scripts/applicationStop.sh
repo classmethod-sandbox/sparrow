bb363bef7db140f11bfaa48c7a2fcadf820affa3
Masao Mochizuki <mochizuki.masao@classmethod.jp>
2017-03-24 16:47:21 +0900

    add spring-boot-actuator

--- scripts/applicationStop.sh
+++ scripts/applicationStop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-service stop sparrow || true
+service sparrow stop || true
--- scripts/monitorService.sh
+++ scripts/monitorService.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 status=down
-while [ "${status}" == "UP" ]; do
+while [ "${status}" != "UP" ]; do
     echo "trying to connect localhost:8080 ..."
-    status=$(curl -s http://localhost:8080/health | jq '.health')
+    status=$(curl -s http://localhost:8080/health | jq -r '.status')
     sleep 5
     echo "current status: ${status}."
 done
