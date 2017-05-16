303ca28e0890456bcf06af5b9fd801d434e55a93
kunita.fumiko <kunita.fumiko@classmethod.jp>
2017-05-16 14:35:50 +0900

    ・フィールドと@Beforeを削除 ・上記に伴い、各テストにsetupを追加 ・見やすさを高めるため、テストを分割

--- src/test/java/jp/classmethod/sparrow/model/CalclatorTest.java
+++ src/test/java/jp/classmethod/sparrow/model/CalclatorTest.java
@@ -19,7 +19,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -34,24 +33,6 @@ import jp.classmethod.sparrow.infrastructure.InMemoryCalculatorRepository;
 @RunWith(MockitoJUnitRunner.class)
 public class CalclatorTest {
 	
-	LineEvent startEvent;
-	
-	LineEvent endEvent;
-	
-	LineEvent resetEvent;
-	
-	LineEvent numberEvent;
-	
-	LineEvent invalidEvent;
-	
-	LineMessageEntity startLineMessageEntity;
-	
-	LineMessageEntity endLineMessageEntity;
-	
-	LineMessageEntity resetLineMessageEntity;
-	
-	LineMessageEntity numberLineMessageEntity;
-	
 	@Spy
 	InMemoryCalculatorRepository inMemoryCalculatorRepository;
 	
@@ -59,52 +40,87 @@ public class CalclatorTest {
 	Calculator sut;
 	
 	
-	@Before
-	public void setup() {
-		// start
-		startEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createStartLineMessage());
-		startLineMessageEntity = LineMessageEntityFixture.createLineEntity(startEvent);
-		
-		// end
-		endEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createEndLineMessage());
-		endLineMessageEntity = LineMessageEntityFixture.createLineEntity(endEvent);
-		
-		// reset
-		resetEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createResetLineMessage());
-		resetLineMessageEntity = LineMessageEntityFixture.createLineEntity(resetEvent);
-		
-		// 数字
-		numberEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createNumberLineMessage());
-		numberLineMessageEntity = LineMessageEntityFixture.createLineEntity(numberEvent);
-		
-		// 無効な文字列
-		invalidEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createInvalidLineMessage());
-	}
-	
+	/**
+	 * start発言時の戻り値を確認します
+	 */
 	@Test
-	public void testSave() {
+	public void testStartSave() {
 		// setup
-		when(inMemoryCalculatorRepository.save(startLineMessageEntity)).thenCallRealMethod();
-		when(inMemoryCalculatorRepository.save(numberLineMessageEntity)).thenCallRealMethod();
-		
+		LineEvent startEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createStartLineMessage());
 		// exesice
-		String result1 = sut.save(startEvent);		// start
-		String result2 = sut.save(endEvent);  		// end
-		String result3 = sut.save(resetEvent);  	// reset
-		String result4 = sut.save(numberEvent);		// 数字
-		String result5 = sut.save(invalidEvent);	// 無効な文字列
-		
+		String actual = sut.save(startEvent);
 		// verify
-		assertThat(result1, is("calc mode start"));
-		assertThat(result2, is("12"));
-		assertThat(result3, is("reset"));
-		assertThat(result4, is(""));
-		assertThat(result5, is("error"));
+		assertThat(actual, is("calc mode start"));
 	}
 	
+	/**
+	 * end発言時の戻り値を確認します
+	 */
+	@Test
+	public void testEndSave() {
+		// setup
+		LineEvent startEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createStartLineMessage());
+		LineEvent numberEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createNumberLineMessage());
+		LineEvent endEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createEndLineMessage());
+		sut.save(startEvent);
+		sut.save(numberEvent);
+		// exesice
+		String actual = sut.save(endEvent);
+		// verify
+		assertThat(actual, is("12"));
+	}
+	
+	/**
+	 * reset発言時の戻り値を確認します
+	 */
+	@Test
+	public void testResetSave() {
+		// setup
+		LineEvent resetEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createResetLineMessage());
+		// exesice
+		String actual = sut.save(resetEvent);
+		// verify
+		assertThat(actual, is("reset"));
+	}
+	
+	/**
+	 * 数字発言時の戻り値を確認します
+	 */
+	@Test
+	public void testNumberSave() {
+		// setup
+		LineEvent numberEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createNumberLineMessage());
+		// exesice
+		String actual = sut.save(numberEvent);
+		// verify
+		assertThat(actual, is(""));
+	}
+	
+	/**
+	 * 仕様外の発言時の戻り値を確認します
+	 */
+	@Test
+	public void testInvalidSave() {
+		// setup
+		LineEvent invalidEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createInvalidLineMessage());
+		// exesice
+		String actual = sut.save(invalidEvent);
+		// verify
+		assertThat(actual, is("error"));
+	}
+	
+	/**
+	 * 引数で渡すLineMessegaEntityのuIdと一致するリストの合計値を返すことを確認します
+	 */
 	@Test
 	public void testCalculateTotal() {
 		// setup
+		LineEvent startEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createStartLineMessage());
+		LineMessageEntity startLineMessageEntity = LineMessageEntityFixture.createLineEntity(startEvent);
+		LineEvent numberEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createNumberLineMessage());
+		LineMessageEntity numberLineMessageEntity = LineMessageEntityFixture.createLineEntity(numberEvent);
+		LineEvent endEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createEndLineMessage());
+		LineMessageEntity endLineMessageEntity = LineMessageEntityFixture.createLineEntity(endEvent);
 		when(inMemoryCalculatorRepository.save(startLineMessageEntity)).thenCallRealMethod();
 		when(inMemoryCalculatorRepository.save(numberLineMessageEntity)).thenCallRealMethod();
 		// exesice
@@ -113,53 +129,107 @@ public class CalclatorTest {
 		assertThat(result, is(12));
 	}
 	
+	/**
+	 * リスト合計値が整数のリストをリセットすることを確認します
+	 */
 	@Test
-	public void testResetList() {
-		// setup　totalがプラスになる
-		when(inMemoryCalculatorRepository.save(startLineMessageEntity)).thenCallRealMethod();
-		when(inMemoryCalculatorRepository.save(numberLineMessageEntity)).thenCallRealMethod();
-		when(inMemoryCalculatorRepository.save(numberLineMessageEntity)).thenCallRealMethod();
-		// exesice
-		Integer result = sut.resetList(resetLineMessageEntity);
-		// verify
-		assertThat(result, is(0));
-	}
-	
-	@Test
-	public void testResetList2() {
-		// setup　totalがマイナスになる
-		numberLineMessageEntity.setValue(-12);
-		when(inMemoryCalculatorRepository.save(startLineMessageEntity)).thenCallRealMethod();
-		when(inMemoryCalculatorRepository.save(numberLineMessageEntity)).thenCallRealMethod();
-		when(inMemoryCalculatorRepository.save(numberLineMessageEntity)).thenCallRealMethod();
+	public void testResetIntegerList() {
 		
+		// setup
+		LineEvent startEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createStartLineMessage());
+		LineMessageEntity startLineMessageEntity = LineMessageEntityFixture.createLineEntity(startEvent);
+		LineEvent numberEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createNumberLineMessage());
+		LineMessageEntity numberLineMessageEntity = LineMessageEntityFixture.createLineEntity(numberEvent);
+		LineEvent resetEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createResetLineMessage());
+		LineMessageEntity resetLineMessageEntity = LineMessageEntityFixture.createLineEntity(resetEvent);
+		when(inMemoryCalculatorRepository.save(startLineMessageEntity)).thenCallRealMethod();
+		when(inMemoryCalculatorRepository.save(numberLineMessageEntity)).thenCallRealMethod();
 		// exesice
 		Integer result = sut.resetList(resetLineMessageEntity);
 		// verify
 		assertThat(result, is(0));
 	}
 	
+	/**
+	 * リスト合計値が数のリストをリセットすることを確認します
+	 */
 	@Test
-	public void testCreateLineMessageEntity() {
+	public void testResetNegativeNumberList() {
 		// setup
-		String messageText1 = startEvent.getMessage().getText();	// start
-		String messageText2 = endEvent.getMessage().getText();		// end
-		String messageText3 = resetEvent.getMessage().getText();	// reset
-		String messageText4 = numberEvent.getMessage().getText();	// 数字
+		LineEvent startEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createStartLineMessage());
+		LineMessageEntity startLineMessageEntity = LineMessageEntityFixture.createLineEntity(startEvent);
+		LineEvent numberEvent =
+				LineEventFixture.createLineUserEvent(LineMessageFixture.createNegativeNumberLineMessage());
+		LineMessageEntity numberLineMessageEntity = LineMessageEntityFixture.createLineEntity(numberEvent);
+		LineEvent resetEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createResetLineMessage());
+		LineMessageEntity resetLineMessageEntity = LineMessageEntityFixture.createLineEntity(resetEvent);
+		when(inMemoryCalculatorRepository.save(startLineMessageEntity)).thenCallRealMethod();
+		when(inMemoryCalculatorRepository.save(numberLineMessageEntity)).thenCallRealMethod();
 		// exesice
-		LineMessageEntity lineMessageEntity5 = sut.createLineMessageEntity(startEvent, messageText1);
-		LineMessageEntity lineMessageEntity6 = sut.createLineMessageEntity(endEvent, messageText2);
-		LineMessageEntity lineMessageEntity7 = sut.createLineMessageEntity(resetEvent, messageText3);
-		LineMessageEntity lineMessageEntity8 = sut.createLineMessageEntity(numberEvent, messageText4);
-		// velify
-		assertThat(lineMessageEntity5.getMessageId(), is("325708"));
-		assertThat(lineMessageEntity5.getUserId(), is("U206d25c2ea6bd87c17655609a1c37cb8"));
-		assertThat(lineMessageEntity5.getTimestamp(), is(146262947912543L));
-		// messageTextによって値が変わるので全パターンテスト
-		assertThat(lineMessageEntity5.getValue(), is(0));
-		assertThat(lineMessageEntity6.getValue(), is(0));
-		assertThat(lineMessageEntity7.getValue(), is(0));
-		assertThat(lineMessageEntity8.getValue(), is(12));
+		Integer result = sut.resetList(resetLineMessageEntity);
+		// verify
+		assertThat(result, is(0));
 	}
 	
+	/**
+	 * start発言時のLineEntity生成を確認します
+	 */
+	@Test
+	public void testCreateStartLineMessageEntity() {
+		// setup
+		LineEvent startEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createStartLineMessage());
+		String messageText = startEvent.getMessage().getText();
+		// exesice
+		LineMessageEntity startLineMessageEntity = sut.createLineMessageEntity(startEvent, messageText);
+		// velify
+		assertThat(startLineMessageEntity.getMessageId(), is("325708"));
+		assertThat(startLineMessageEntity.getUserId(), is("U206d25c2ea6bd87c17655609a1c37cb8"));
+		assertThat(startLineMessageEntity.getTimestamp(), is(146262947912543L));
+		assertThat(startLineMessageEntity.getValue(), is(0));
+	}
+	
+	/**
+	 * end発言時のLineEntity生成を確認します
+	 */
+	@Test
+	public void testCreateEndLineMessageEntity() {
+		// setup
+		LineEvent endEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createEndLineMessage());
+		String messageText = endEvent.getMessage().getText();
+		// exesice
+		LineMessageEntity endLineMessageEntity = sut.createLineMessageEntity(endEvent, messageText);
+		// velify
+		// getValue以外はtestCreateStartLineMessageEntityと同じなので割愛
+		assertThat(endLineMessageEntity.getValue(), is(0));
+	}
+	
+	/**
+	 * reset発言時のLineEntity生成を確認します
+	 */
+	@Test
+	public void testCreateResetLineMessageEntity() {
+		// setup
+		LineEvent resetEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createResetLineMessage());
+		String messageText = resetEvent.getMessage().getText();
+		// exesice
+		LineMessageEntity resetLineMessageEntity = sut.createLineMessageEntity(resetEvent, messageText);
+		// velify
+		// getValue以外はtestCreateStartLineMessageEntityと同じなので割愛
+		assertThat(resetLineMessageEntity.getValue(), is(0));
+	}
+	
+	/**
+	 * 数字発言時のLineEntity生成を確認します
+	 */
+	@Test
+	public void testCreateNumberLineMessageEntity() {
+		// setup
+		LineEvent numberEvent = LineEventFixture.createLineUserEvent(LineMessageFixture.createNumberLineMessage());
+		String messageText = numberEvent.getMessage().getText();
+		// exesice
+		LineMessageEntity numberLineMessageEntity = sut.createLineMessageEntity(numberEvent, messageText);
+		// velify
+		// getValue以外はtestCreateStartLineMessageEntityと同じなので割愛
+		assertThat(numberLineMessageEntity.getValue(), is(12));
+	}
 }
